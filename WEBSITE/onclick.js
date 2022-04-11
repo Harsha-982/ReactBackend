db34fd88b838a622c5ff0f7a06586d9c0b72c1bd
Harsha-982 <82398710+Harsha-982@users.noreply.github.com>
2022-04-11 15:11:50 +0530

    Add files via upload

---
+++ WEBSITE/onclick.js
@@ -0,0 +1,5 @@
+function onclick1(){
+    document.getElementById('firstimage').setAttribute("style","background-image=url('')");
+}
+
+document.getElementById('firstimage').addEventListener("click",onclick1);
